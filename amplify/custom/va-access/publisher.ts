0c780c7aa1b01e12d3cc9d13726c46bb14e2a620
Steve Meyer <stevenmeyer142@gmail.copm>
2025-03-19 14:55:56 -0400

    trying to get typescript to compile

--- amplify/custom/va-access/publisher.ts
+++ amplify/custom/va-access/publisher.ts
@@ -2,12 +2,18 @@
 import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
 import type { Handler } from 'aws-lambda';
 import type { Message } from './resource'; 
+import { readdir } from 'fs/promises';
+import { join } from 'path';
 
 const client = new SNSClient({ region: process.env.AWS_REGION });
 exports.handler = async () => {
+  const currentDirectory = process.cwd();
+  const files = await readdir(currentDirectory);
+
+  console.log('Current directory files:', files);
   return {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
-      body: JSON.stringify({ message: "Hello, World!" }),
+      body: JSON.stringify({ message: "Hello, World!",files : files }),
   };
 };
--- amplify/custom/va-access/resource.ts
+++ amplify/custom/va-access/resource.ts
@@ -1,5 +1,5 @@
 import * as url from 'node:url';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import apigateway  from 'aws-cdk-lib/aws-apigateway';
 import apigwv2, { HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
@@ -16,16 +16,18 @@ export type Message = {
 export class CustomNotifications extends Construct {
     public readonly topic: string;
     public readonly gateway_url : string;
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string) { 
     super(scope, id);
 
     this.topic = "arn:aws:sns:us-east-1:123456789012:MyTopic";
 
     // Create Lambda to publish messages to SNS topic
     const publisher = new lambda.NodejsFunction(this, 'Publisher', {
-      entry: url.fileURLToPath(new URL('publisher.ts', import.meta.url)),
+      handler: 'lambda.handler',
+      code: Code.fromAsset('amplify/custom/va-access'), 
+      memorySize: 1024, 
 
-      runtime: Runtime.NODEJS_18_X
+      runtime: Runtime.NODEJS_LATEST
     });
 
     
@@ -35,7 +37,7 @@ const booksIntegration = new HttpLambdaIntegration('BooksIntegration', publisher
 
 const httpApi = new apigwv2.HttpApi(this, 'HttpApi');
 
-httpApi.addRoutes({
+httpApi.addRoutes({ 
   path: '/',
   integration: booksIntegration,
 });
